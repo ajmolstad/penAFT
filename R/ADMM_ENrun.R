022fe99174da4e1a05d25d8aec25650afc75324c
PIOTRSUDER99 <44028504+PIOTRSUDER99@users.noreply.github.com>
2020-10-13 23:15:45 -0400

    Add files via upload

--- R/ADMM_ENrun.R
+++ R/ADMM_ENrun.R
@@ -1,11 +1,18 @@
-ADMM.ENrun <- function(tildelogY, X, D, tildedelta, rho, eta, tau, lambda, alpha, w, Gamma, Beta, Theta, max.iter, tol.abs, tol.rel, gamma, euc.tildelogY, max.iter.update) {
-  p <- dim(X)[2]
-  n <- dim(X)[1]
-  l <- length(tildelogY)
-  Xbeta <- crossprod(t(X), Beta)
-  tXB <-  crossprod(t(D),Xbeta)
-  #D <- Matrix(D, sparse=TRUE)
-
-  out <- ADMM_ENrun(tildelogY, X, D, tildedelta, rho, eta, tau, lambda, alpha, w, Gamma, Beta, Theta, max_iter = max.iter, tol_abs = tol.abs, tol_rel = tol.rel, gamma, euc_tildelogY = euc.tildelogY, Xbeta, tXB, n, l, p, max_iter_update = max.iter.update)
+ADMM.ENrun <- function(tildelogY, X, D, tildedelta, rho, eta, tau, 
+                       lambda, alpha, w, Gamma, Beta, 
+                       Theta,  
+                       max.iter, tol.abs, tol.rel, gamma, euc.tildelogY){
+  max.iter <- 1
+  out <- ADMM_ENrun(tildelogY, X, D, tildedelta, rho, eta, tau, 
+             lambda, alpha, w, Gamma, Beta, 
+             Theta,  
+             max.iter, tol.abs, tol.rel, gamma, euc.tildelogY)
+  
+  if(out$iter.counter == max.iter){
+    warning("ADMM did not converge in max.iter iterations", "\n")
+  }
+  
+  print(out$Theta[out$Theta != 0][1:30])
+  
   out
 }
