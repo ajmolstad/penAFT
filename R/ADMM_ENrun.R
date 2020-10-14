5471625c17781a774204bad429429158469846eb
PIOTRSUDER99 <44028504+PIOTRSUDER99@users.noreply.github.com>
2020-10-13 23:48:56 -0400

    Add files via upload

--- R/ADMM_ENrun.R
+++ R/ADMM_ENrun.R
@@ -2,7 +2,8 @@ ADMM.ENrun <- function(tildelogY, X, D, tildedelta, rho, eta, tau,
                        lambda, alpha, w, Gamma, Beta, 
                        Theta,  
                        max.iter, tol.abs, tol.rel, gamma, euc.tildelogY){
-  max.iter <- 1
+
+
   out <- ADMM_ENrun(tildelogY, X, D, tildedelta, rho, eta, tau, 
              lambda, alpha, w, Gamma, Beta, 
              Theta,  
@@ -11,8 +12,6 @@ ADMM.ENrun <- function(tildelogY, X, D, tildedelta, rho, eta, tau,
   if(out$iter.counter == max.iter){
     warning("ADMM did not converge in max.iter iterations", "\n")
   }
-  
-  print(out$Theta[out$Theta != 0][1:30])
   
   out
 }
